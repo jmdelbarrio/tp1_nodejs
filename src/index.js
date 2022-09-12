aa9109765ec3acc124a72c8ba9b9371135d3735c
jmdelbarrio <jmdelbarrio@gmail.com>
2022-09-12 13:50:31 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,17 +5,50 @@ app.use(express.urlencoded({extended:true}));
 
 // -------------- RUTAS --------------
 
-// cuando es GET recibo el dato por la url
-app.get('/pacientes/:nombre', (req, res) => { // req: request, res: response
-    res.send('Hola ' + req.params.nombre); // envia una respuesta y extrae el nombre del parámetro
+// retorna el paciente con ID :id si es que existe
+app.get('/pacientes/:id', (req, res) => { // req: request, res: response
+    res.send('Retorna el paciente ID: ' + req.params.id); 
 });
 
-// cuando es POST recibo el dato por el BODY
-app.post('/pacientes', (req, res) => { // req: request, res: response
-    console.log(req.body);
-    res.send('Hola ' + req.body.nombre); // envia una respuesta y extraigo el nombre del body
+// retorna todos los pacientes
+app.get('/pacientes', (req, res) => { // req: request, res: response
+    res.send('Retorna todos los pacientes'); 
 });
 
+// da de alta un paciente
+app.post('/paciente', (req, res) => { // req: request, res: response    
+    res.send('Alta de paciente OK\nApellido: ' + req.body.apellido +'\nNombres: ' + req.body.nombres);    
+});
+
+// retorna el medico con ID :id si es que existe
+app.get('/medicos/:id', (req, res) => { // req: request, res: response
+    res.send('Retorna el medico ID: ' + req.params.id); 
+});
+
+// retorna todos los medicos
+app.get('/pacientes', (req, res) => { // req: request, res: response
+    res.send('Retorna todos los medicos'); 
+});
+
+// da de alta un medico
+app.post('/medico', (req, res) => { // req: request, res: response    
+    res.send('Alta de medico OK\nApellido: ' + req.body.apellido +'\nNombres: ' + req.body.nombres+'\ID Especialidad: ' + req.body.especialidadID);
+});
+
+// retorna la prestacion con ID :id si es que existe
+app.get('/prestaciones/:id', (req, res) => { // req: request, res: response
+    res.send('Retorna la prestacion ID: ' + req.params.id); 
+});
+
+// retorna todas las prestaciones
+app.get('/prestaciones', (req, res) => { // req: request, res: response
+    res.send('Retorna todas las prestaciones'); 
+});
+
+// da de alta una prestacion
+app.post('/prestacion', (req, res) => { // req: request, res: response    
+    res.send('Alta de prestacion OK\nNombre: ' + req.body.prestacion);    
+});
 
 // ---------- PUERTO ----------
 app.listen(5000); // escucha en el puerto 5000
