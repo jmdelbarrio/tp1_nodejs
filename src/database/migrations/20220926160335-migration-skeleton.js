179cf0e767e2a146cc441716a633f88a6009c743
jmdelbarrio <jmdelbarrio@gmail.com>
2022-09-26 13:04:49 -0300

    migrations

--- src/database/migrations/20220926160335-migration-skeleton.js
+++ src/database/migrations/20220926160335-migration-skeleton.js
@@ -2,23 +2,22 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.transaction(t => {
+    
         return Promise.all([
           queryInterface.addColumn('paciente', 'email', {
-            type: Sequelize.DataTypes.STRING
+            type: Sequelize.DataTypes.STRING,
+            allowNull: true
           }, { transaction: t }),
           queryInterface.addColumn('medico', 'email', {
             type: Sequelize.DataTypes.STRING,
+            allowNull: true
           }, { transaction: t })
-        ]);
-      });
+        ]);    
   },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.transaction(t => {
+  down: (queryInterface, Sequelize) => {    
         return Promise.all([
           queryInterface.removeColumn('paciente', 'email', { transaction: t }),
           queryInterface.removeColumn('medico', 'email', { transaction: t })
-        ]);
-      });
+        ]);      
   }
 }
