518c7a8e793e3f3d1dd0b865ab471dd258dd5b5d
jmdelbarrio <jmdelbarrio@gmail.com>
2022-09-26 15:35:56 -0300

    Update paciente.controller.js

--- src/controllers/paciente.controller.js
+++ src/controllers/paciente.controller.js
@@ -1,34 +1,72 @@
 //CONTROLADOR DE PACIENTE
 
+const models = require('../database/models/index');
+
 module.exports = {
     listar: async (req,res) =>{
-        try {
-            console.log('ejecutando listar');
+        try {                        
+            const pacientes = await models.paciente.findAll();
             res.json({
-                message: "Listar información de todos"
+                success: true,
+                message: "Listar información de todos",
+                data:{
+                    pacientes: pacientes
+                }
             })
         } catch (error) {
             console.log(error);
-        }            
+        }                    
     },
     crear: async (req,res) =>{
         try {
-            console.log('ejecutando crear');
+            
+            const paciente = await models.paciente.create({
+                nombre: req.body.nombre,
+                apellidos: req.body.apellidos,
+                email: req.body.email,
+            })
+
             res.json({
-                message: "Crear Paciente - Nombres: "+req.body.nombres+" Apellido: "+req.body.apellido
+                success: true,
+                message: "Crear Paciente - Nombres: "+req.body.nombre+" Apellidos: "+req.body.apellidos,
+                data:{
+                    id: paciente.id
+                }
             })
         } catch (error) {
             console.log(error);
-        }        
+            res.json({
+                success: false,
+                message: "Error al crear paciente",
+                data:{
+                    error: error//.name
+                }
+            })
+        }      
     },
     listarInfo: async (req,res) =>{
         try {
-            console.log('ejecutando listarInfo');
-            res.json({
-                message: "Listar información de 1 - ID:" +req.params.idPaciente
+            const paciente = await models.paciente.findOne({
+                where:{
+                    id: req.params.idPaciente
+                }
+            })
+            res.json({                
+                success: true,
+                message: "Listar información de 1 - ID:" +req.params.idPaciente,
+                data:{
+                    paciente: paciente
+                }
             })
         } catch (error) {
             console.log(error);
+            res.json({
+                success: false,
+                message: "Error al buscar Paciente",
+                data:{
+                    error: error.name
+                }
+            })
         }
     },    
 }
